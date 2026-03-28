1202229be984ce0a851b59f6462b090400da9cf5
Boomer-95 <roldan2943@gmail.com>
2026-03-27 21:51:16 -0600

    Ajuste final del despertador con secretos

--- robot_dolar.py
+++ robot_dolar.py
@@ -1,6 +1,7 @@
+import os
+import json
 import requests
 import gspread
-
 # ==========================================
 # 🔧 Configuración
 # ==========================================
@@ -31,11 +32,16 @@ except Exception as e:
     print(f"❌ Error al leer de Banxico: {e}")
     exit()
 
-# ==========================================
-# 📝 2. Escribir en Google Sheets
-# ==========================================
 try:
-    cliente = gspread.service_account(filename=ARCHIVO_JSON)
+    # 🕵️‍♂️ ¿Estamos en GitHub o en tu computadora local?
+    if "GOOGLE_CREDENTIALS" in os.environ:
+        # Si estamos en GitHub, saca la llave del Secreto
+        info_credenciales = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+        cliente = gspread.service_account_from_dict(info_credenciales)
+    else:
+        # Si estás en tu PC local, usa tu archivo credenciales.json tradicional
+        cliente = gspread.service_account(filename="credenciales.json")
+
     hoja = cliente.open_by_key(ID_HOJA_CALCULO).worksheet("Hoja 1")
     
     fechas_existentes = hoja.col_values(1)
